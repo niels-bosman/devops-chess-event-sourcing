7b8c9b985877d3f989c4a405aaaf504ade8a54f5
Niels Bosman <nielsbosman@mail.com>
2022-09-19 14:11:19 +0200

    feat: add constructor to move made to make it initializable

--- src/events/MoveMade.ts
+++ src/events/MoveMade.ts
@@ -5,4 +5,10 @@ import { Event } from './Event'
 export class MoveMade extends Event {
   public move: Move
   public piece: Piece
+
+  constructor(move: Move, piece: Piece) {
+    super()
+    this.move = move
+    this.piece = piece
+  }
 }
