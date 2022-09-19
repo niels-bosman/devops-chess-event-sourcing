105562a0b0452754ab303a19082db16b5db68fd4
Niels Bosman <nielsbosman@mail.com>
2022-09-19 14:07:34 +0200

    fix: import correct Event

--- src/events/MoveMade.ts
+++ src/events/MoveMade.ts
@@ -1,5 +1,6 @@
 import { Move } from '../domain/entities/Move'
 import { Piece } from '../domain/entities/Piece'
+import { Event } from './Event'
 
 export class MoveMade extends Event {
   public move: Move
