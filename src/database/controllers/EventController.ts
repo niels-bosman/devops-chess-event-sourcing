c549e8d19b66eb37d2daf28391a0915192e8da97
Mike van Egmond <mikevanegmond1998@outlook.com>
2022-09-19 15:03:05 +0200

    Added small fix

--- src/database/controllers/EventController.ts
+++ src/database/controllers/EventController.ts
@@ -2,10 +2,15 @@ import { Event } from '../../events/Event'
 import EventModel from '../models/event'
 
 export class EventController {
+
   static saveEvent(event: Event) {
+
+    console.log("Saving event: " + event.toString());
+
     new EventModel({
-      event: event.name,
-      data: event.toString(),
+      Type: event.name,
+      EventData: event.toString(),
     }).save()
   }
+  
 }
