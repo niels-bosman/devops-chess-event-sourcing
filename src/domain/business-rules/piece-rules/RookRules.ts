e8f849ea7a8aa4bc6f04cf5a5373aa3cfcb14b4c
Mike van Egmond <mikevanegmond1998@outlook.com>
2022-09-19 14:10:01 +0200

    added missing Rook rule.

--- src/domain/business-rules/piece-rules/RookRules.ts
+++ src/domain/business-rules/piece-rules/RookRules.ts
@@ -2,4 +2,8 @@ export class RookRules {
   static directionIsAllowed(piece: any, move: any): boolean {
     return true;
   }
+  
+  static MoveIsAllowedOnboard(piece: any, move: any, board: any): boolean {
+    return true;
+  }
 }
